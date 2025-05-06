d8eeae94750718e9155583e2f631f827c1064fc7
Antonella Badami <cantobadami@gmail.com>
2025-05-06 12:14:22 -0300

    splash screen añadido

--- app/src/main/java/com/tecmov2025/manoslocales/LoginActivity.kt
+++ app/src/main/java/com/tecmov2025/manoslocales/LoginActivity.kt
@@ -1,31 +1,77 @@
 package com.tecmov2025.manoslocales
 
 import android.os.Bundle
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.delay
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Login()
+            val navController = rememberNavController()
+            Nav(navController)
+        }
+    }
+
+    @Composable
+    fun Nav(navController: NavHostController)
+    {
+
+        NavHost(navController = navController, startDestination = "SplashScreen")
+        {
+            composable("Login")
+            {
+                Login()
+            }
+            composable("SplashScreen")
+            {
+                SplashScreen(navController)
+            }
+        }
+    }
+
+    @Composable
+    fun SplashScreen(navController: NavHostController)
+    {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Blue)
+        ){
+          Text("Manos Locales")
+        }
+        LaunchedEffect(Unit)
+        {
+            delay(3000)
+            navController.navigate("Login")
         }
     }
 
@@ -36,7 +82,9 @@ class LoginActivity : ComponentActivity() {
             modifier = Modifier
                 .fillMaxSize(),
             horizontalAlignment = Alignment
-                .CenterHorizontally
+                .CenterHorizontally,
+            verticalArrangement = Arrangement
+                .Center
         ) {
             var username by remember { mutableStateOf("") }
             var password by remember { mutableStateOf("") }
@@ -48,6 +96,10 @@ class LoginActivity : ComponentActivity() {
                 value = password,
                 onValueChange = { password = it }
             )
+            Button(onClick = {}) {
+                Text("iniciar sesion")
+            }
+
         }
 
     }
