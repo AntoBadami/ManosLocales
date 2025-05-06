713ebf6cd6ac766071cb0cb8f9e5b6fa7f2e920f
Antonella Badami <cantobadami@gmail.com>
2025-05-06 12:14:22 -0300

    Maquetado de Login completo

--- app/src/main/java/com/tecmov2025/manoslocales/LoginActivity.kt
+++ app/src/main/java/com/tecmov2025/manoslocales/LoginActivity.kt
@@ -5,12 +5,18 @@ import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -23,6 +29,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kotlinx.coroutines.delay
@@ -45,16 +57,12 @@ class LoginActivity : ComponentActivity() {
     fun Nav(navController: NavHostController)
     {
 
-        NavHost(navController = navController, startDestination = "SplashScreen")
+        NavHost(
+            navController = navController,
+            startDestination = "SplashScreen")
         {
-            composable("Login")
-            {
-                Login()
-            }
-            composable("SplashScreen")
-            {
-                SplashScreen(navController)
-            }
+            composable("Login") { Login() }
+            composable("SplashScreen") { SplashScreen(navController) }
         }
     }
 
@@ -70,7 +78,7 @@ class LoginActivity : ComponentActivity() {
         }
         LaunchedEffect(Unit)
         {
-            delay(3000)
+            delay(500)
             navController.navigate("Login")
         }
     }
@@ -84,24 +92,83 @@ class LoginActivity : ComponentActivity() {
             horizontalAlignment = Alignment
                 .CenterHorizontally,
             verticalArrangement = Arrangement
-                .Center
+                .spacedBy(20.dp, alignment = Alignment.CenterVertically)
         ) {
             var username by remember { mutableStateOf("") }
             var password by remember { mutableStateOf("") }
-            TextField (
-                value = username,
-                onValueChange = { username = it }
+
+            Image (
+                modifier = Modifier
+                    .size(150.dp),
+                painter = painterResource(R.drawable.logo),
+                contentDescription = "Logo",
+                contentScale = ContentScale.Fit
             )
-            TextField (
-                value = password,
-                onValueChange = { password = it }
-            )
-            Button(onClick = {}) {
-                Text("iniciar sesion")
+            CustomTextField(
+                username,
+                { username = it },
+                "Usuario")
+
+            CustomTextField(
+                password,
+                { password = it },
+                "Contraseña",
+                true)
+
+            Button(
+                modifier = Modifier
+                    .height(48.dp)
+                    .width(324.dp),
+                onClick = {}) {
+                Text("Iniciar sesión")
             }
+
+            LinkText("No tenes cuenta? - Registrate", {})
+            LinkText("Recuperar contraseña",{})
 
         }
 
+    }
+
+    /**
+     * Campo de texto personalizado que puede ocultar la entrada (para contraseñas).
+     *
+     * @param value El valor del campo de texto.
+     * @param onValueChange Función para manejar el cambio de valor.
+     * @param label El texto de la etiqueta.
+     * @param isPassword Indica si el campo debe ocultar la entrada como una contraseña. Por defecto es `false`.
+     */
+    @Composable
+    fun CustomTextField(
+        value: String,
+        onValueChange: (String) -> Unit,
+        label: String,
+        isPassword: Boolean = false
+    )
+    {
+        TextField (
+            modifier = Modifier
+                .width(324.dp)
+                .height(48.dp),
+            value = value,
+            onValueChange = onValueChange,
+            label = { Text(label) },
+            visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
+            keyboardOptions = if (isPassword)  KeyboardOptions(keyboardType = KeyboardType.Password) else KeyboardOptions.Default
+        )
+
+    }
+
+    @Composable
+    fun LinkText(text: String,
+                 onClick: ()-> Unit
+    )
+    {
+        Text(
+            modifier = Modifier
+                .clickable{onClick()},
+            text = text,
+        )
     }
 
 }
