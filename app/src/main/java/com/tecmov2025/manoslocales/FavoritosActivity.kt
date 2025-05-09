7ac1a95dbc20c98af8015acef070fbd9e96a634b
Antonella Badami <cantobadami@gmail.com>
2025-05-09 11:07:18 -0300

    lazy columns en main y en favoritos

--- app/src/main/java/com/tecmov2025/manoslocales/FavoritosActivity.kt
+++ app/src/main/java/com/tecmov2025/manoslocales/FavoritosActivity.kt
@@ -1,13 +1,113 @@
 package com.tecmov2025.manoslocales
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardDefaults.cardElevation
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.tecmov2025.manoslocales.ui.theme.ManosLocalesTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 
-class FavoritosActivity : AppCompatActivity() {
+
+class FavoritosActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ManosLocalesTheme {
+                val navController = rememberNavController()
+                Nav(navController)
+            }
+        }
     }
+
+    @Composable
+    fun Nav(navController: NavHostController) {
+        NavHost(
+            navController = navController,
+            startDestination = "FavoritoScreen"
+        )
+        {
+            composable("FavoritoScreen") { FavoritoScreen(navController) }
+        }
+    }
+
+    @Composable
+    fun FavoritoScreen(navController: NavHostController) {
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ){
+            items(30) { index ->
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = CardDefaults.cardColors(containerColor = Color.LightGray),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
+                        // Cuadro para la imagen del producto
+                        Box(
+                            modifier = Modifier
+                                .size(64.dp)
+                                .background(Color.DarkGray)
+                        )
+
+                        // Espacio entre imagen y texto
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        // Texto descriptivo
+                        Column {
+                            Text(
+                                text = "Producto $index",
+                                style = MaterialTheme.typography.titleMedium
+                            )
+                            Text(
+                                text = "Descripcion del producto $index",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
 }
+
---
+++ app/src/main/java/com/tecmov2025/manoslocales/MainScreen.kt
@@ -0,0 +1,143 @@
+package com.tecmov2025.manoslocales
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+// Clase Producto
+data class Producto(val nombre: String, val descripcion: String)
+
+@Composable
+fun FavoritoScreen(navController: NavHostController) {
+    //lista de productos
+    val productosMock = List(30) { index ->
+        Producto(
+            nombre = "Producto $index",
+            descripcion = "Descripción del producto $index"
+        )
+    }
+    //productos en pares
+    val productosAgrupados = productosMock.chunked(2)
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(productosAgrupados) { grupo ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp) //espacio entre productos
+            ) {
+                //primer producto
+                Box(modifier = Modifier.weight(1f)) {
+                    ProductoCard(producto = grupo[0])
+                }
+                //segundo producto
+                if (grupo.size > 1) {
+                    Box(modifier = Modifier.weight(1f)) {
+                        ProductoCard(producto = grupo[1])
+                    }
+                }
+            }
+        }
+    }
+    /*LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        items(30) { index ->
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                colors = CardDefaults.cardColors(containerColor = Color.LightGray),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    // Cuadro para la imagen del producto
+                    Box(
+                        modifier = Modifier
+                            .size(64.dp)
+                            .background(Color.DarkGray)
+                    )
+
+                    // Espacio entre imagen y texto
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    // Texto descriptivo
+                    Column {
+                        Text(
+                            text = "Producto $index",
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                        Text(
+                            text = "Descripcion del producto $index",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+            }
+        }
+    }*/
+
+}
+@Composable
+fun ProductoCard(producto: Producto) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(containerColor = Color.LightGray),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            //espacio para la imagen del producto
+            Box(
+                modifier = Modifier
+                    .size(64.dp)
+                    .background(Color.DarkGray)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = producto.nombre,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = producto.descripcion,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
