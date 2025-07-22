5629f39d26bf04aa878c64bf423f997daf9de491
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-07-22 18:49:26 -0300

    Solucion de Bugs

--- app/src/main/java/com/tecmov2025/manoslocales/Utils/FavoritosMapper.kt
+++ app/src/main/java/com/tecmov2025/manoslocales/Utils/FavoritosMapper.kt
@@ -3,7 +3,7 @@ package com.tecmov2025.manoslocales.Utils
 import com.tecmov2025.manoslocales.Database.Entity.FavoritoEntity
 import com.tecmov2025.manoslocales.Networking.FavoritoDTO
 
-fun FavoritoDTO.toEntity(): FavoritoEntityEntity {
+fun FavoritoDTO.toEntity(): FavoritoEntity {
     return FavoritoEntity(
         id = this.id,
         producto = this.producto
