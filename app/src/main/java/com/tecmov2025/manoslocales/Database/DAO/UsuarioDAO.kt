d2c02601ffb141dc3cc7eacdb7abf1a60ccdac78
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-07-24 20:21:36 -0300

    Tabla Usuario creada en DB

---
+++ app/src/main/java/com/tecmov2025/manoslocales/Database/DAO/UsuarioDAO.kt
@@ -0,0 +1,28 @@
+package com.tecmov2025.manoslocales.Database.DAO
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.tecmov2025.manoslocales.Database.Entity.UsuarioEntity
+
+@Dao
+interface UsuarioDAO {
+
+    @Query("SELECT * FROM UsuarioEntity LIMIT 1")
+    fun getUsuario(): UsuarioEntity
+
+    // Cambiar contraseña
+    @Query("UPDATE UsuarioEntity SET pass = :nuevaPass WHERE id = :id")
+    fun actualizarPassword(id: Int, nuevaPass: String)
+
+    // Cambiar email
+    @Query("UPDATE UsuarioEntity SET email = :nuevoEmail WHERE id = :id")
+    fun actualizarEmail(id: Int, nuevoEmail: String)
+
+    // Cambiar nombre
+    @Query("UPDATE UsuarioEntity SET nombre = :nuevoNombre WHERE id = :id")
+    fun actualizarNombre(id: Int, nuevoNombre: String)
+
+    // Cambiar apellido
+    @Query("UPDATE UsuarioEntity SET apellido = :nuevoApellido WHERE id = :id")
+    fun actualizarApellido(id: Int, nuevoApellido: String)
+}
