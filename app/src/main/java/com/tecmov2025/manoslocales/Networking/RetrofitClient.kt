265139a889514a3651b0b8bbf21d694ed6ec6285
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-07-28 14:37:27 -0300

    Actualizacion de servidor Mock

--- app/src/main/java/com/tecmov2025/manoslocales/Networking/RetrofitClient.kt
+++ app/src/main/java/com/tecmov2025/manoslocales/Networking/RetrofitClient.kt
@@ -5,8 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient{
     //private const val BASE_URL = "https://manos-locales.free.beeceptor.com"
-    private const val BASE_URL = "https://manoslocalesapp.app.smartmock.io"
-
+    //private const val BASE_URL = "https://manoslocalesapp.app.smartmock.io"
+    private const val BASE_URL = "https://manoslocales.app.smartmock.io"
     val apiService: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
